a67bec0ee8949b1f9d91839e23b1dae00e40ce4d
Boubacar Ouattara <boubacar.ouattara@biblibre.com>
2024-04-05 13:45:27 +0200

    AddAvis

---
+++ Server/src/v1/routes/avisRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const authMiddleware = require("../../middlewares/middleware");
+const avisController = require("../controllers/avisController");
+
+const avisRouter = express.Router();
+
+// Publier un avis
+avisRouter.post("/", avisController.createAvis);
+
+// obtenir les avis
+avisRouter.get("/", avisController.getAllAvis);
+
+// obtenir un avis par Id
+avisRouter.get("/:id", avisController.getAvisById);
+
+// MAJ d'un avis uniquement si l'utilisateur est autorisé
+avisRouter.put("/:id", avisController.updateAvis);
+
+// Supprimer un avis uniquement si l'utilisateur est autorisé
+avisRouter.delete("/:id", avisController.deleteAvis);
+
+module.exports = avisRouter;
